283eeef6248b78f6d771df84ecc9e261435c43d5
gkuling <34655398+gkuling@users.noreply.github.com>
2024-11-12 20:37:27 -0500

    Updated the quiz generator to use Bloom's Taxonomy

--- generate_quiz.py
+++ generate_quiz.py
@@ -1,21 +1,14 @@
-import random
-
 from course_time_table import course_schedule
 import os
 import pandas as pd
 import argparse
 
 import numpy as np
-from llama_index.core import SimpleDirectoryReader, Settings, VectorStoreIndex, \
-    load_index_from_storage
+from llama_index.core import Settings, load_index_from_storage
 from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
 from llama_index.core import StorageContext
 from llama_index.llms.azure_openai import AzureOpenAI
-from llama_index.core.extractors import (
-    TitleExtractor,
-    QuestionsAnsweredExtractor,
-)
-from llama_index.core.node_parser import TokenTextSplitter
+from llama_index.core.llms import ChatMessage
 
 import dotenv
 dotenv.load_dotenv('.env')
@@ -40,64 +33,145 @@ Settings.embed_model = AzureOpenAIEmbedding(
 
 llm = Settings.llm
 
+### Setting up the RAG Model for knowledge base
+
+vec_store = r"C:\Project_Data\AIM2_project\course_pdf_data\vec_store"
+
+print('Loading index from vec_store...')
+# load index from vec_store if it exists
+storage_context = StorageContext.from_defaults(
+    persist_dir=vec_store)
+
+# load index
+rag = load_index_from_storage(storage_context)
+rag = rag.as_query_engine()
+
+def generate_quiz_question_prompt(topic, skill_level):
+    if skill_level == "Knowledge":
+        # ask RAG for a definition
+        facts = rag.query(
+            f"Give me a key definition that would be in a textbook for {topic}"
+        )
+
+        # Ask LLM to generate a knowledge-based question
+        prompt = (
+            f"Given the facts: \n{facts} \nGenerate a question and the answer that tests students' "
+            f"knowledge on the definition of {topic}."
+        )
+        # Expected outcome: Questions like “What is the definition of [topic]?”
+    elif skill_level == "Understanding":
+        # ask RAG for a simple explanation
+        facts = rag.query(
+            f"Give me a simple explanation of {topic}"
+        )
+        # Ask LLM to generate a comprehension-based question
+        prompt = (
+            f"Given the facts: \n{facts} \nGenerate a question and the answer "
+            f"that asks students to explain the concept of {topic} in their own words."
+        )
+        # Expected outcome: Questions like “Explain in your own words what [topic] means.”
+    elif skill_level == "Applying":
+        # ask RAG for a real-world scenario
+        facts = rag.query(
+            f"Give me a real-world scenario where {topic} is applied"
+        )
+
+        # Ask LLM to generate an application-based question
+        prompt = (
+            f"Given the facts: \n{facts} \nGenerate a question and the answer "
+            f"that asks students how they would apply {topic} in a real-world "
+            f"scenario based on the facts."
+        )
+        # Expected outcome: Questions like “How would you use [topic] to solve a problem in [relevant field]?”
+    elif skill_level == "Analyzing":
+        # ask RAG for a comparison
+        facts = rag.query(
+            f"Give me a comparison of {topic} and something similar. "
+        )
+        # Ask LLM to generate a question that requires analysis and multiple perspectives
+        prompt = (
+            f"Given the facts: \n{facts} \nGenerate a question and the answer asking students to compare and contrast different aspects of {topic}."
+        )
+        # Expected outcome: Questions like “What are the differences between [aspect 1] and [aspect 2] in [topic]? Which factors influence these differences?”
+    elif skill_level == "Creating":
+        # ask RAG for a design-based question
+        facts = rag.query(
+            f"Give me a scenario where {topic} is used to design something new."
+        )
+        # Ask LLM to generate a creative and design-based question
+        prompt = (
+            f"Given the facts: \n{facts} \nGenerate a question and the answer that asks students to design or propose a new solution based on {topic}."
+        )
+        # Expected outcome: Questions like “How would you design a solution for [specific problem] using [topic]? What challenges would you anticipate?”
+    elif skill_level == "Evaluating":
+        # ask RAG for an evaluation-based question
+        facts = rag.query(
+            f"Give me a scenario where {topic} is evaluated for its effectiveness."
+        )
+        # Ask LLM to generate an evaluation-based question
+        prompt = (
+            f"Given the facts: \n{facts} \nGenerate a question and the answer that asks students to evaluate the effectiveness of {topic} in a given context."
+        )
+        # Expected outcome: Questions like “How effective is [topic] in solving [specific problem]? What are the limitations?”
+    else:
+        raise ValueError(f"Invalid skill level: {skill_level}")
+    return prompt
 def generate_quiz(pdf_folder_path, week_number, num_questions, output_local):
-    # Check if vec_store exists
-    vec_store = os.path.join(
-        pdf_folder_path, "vec_store"
-    )
 
-    print('Loading index from vec_store...')
-    # load index from vec_store if it exists
-    storage_context = StorageContext.from_defaults(
-        persist_dir=vec_store)
-
-    # load index
-    index = load_index_from_storage(storage_context)
-    # Function to generate quiz questions based on week number
+    # get week data
+    question_options = []
     week_data = next((week for week in course_schedule["weeks"] if week["week"] == week_number), None)
-    if not week_data:
-        print(f"Week {week_number} not found in the course schedule.")
-        return
+    week_data['topics'] = week_data['concepts'] + week_data['required_readings']
+    # generate question options based on topics and skill levels
+    for topic in week_data["topics"]:
+        # Add different skill levels for each topic based on Bloom's Taxonomy
+        for skill_level in ["Knowledge", "Understanding", "Applying",
+                            "Analyzing", "Evaluating", "Creating"]:
+            question_options.append(
+                {'topic': topic, 'skill_level': skill_level}
+            )
+    # Calculate the number of times we need to repeat the list
+    repeat_count = int(np.ceil(num_questions / len(question_options)))
 
-    title = week_data["title"]
-    concepts = week_data["concepts"]
-    readings = week_data["required_readings"]
+    # Repeat the list and slice to get the exact target length
+    question_options = (question_options * repeat_count)
+    np.random.shuffle(question_options)
+    question_options = question_options[:num_questions]
 
-    questions_answers = []
+    questions_list = []
+    for i, question in enumerate(question_options):
+        concept = question['topic']
+        taxonomy = question['skill_level']
 
-    for i in range(num_questions):
-        # Randomly select a concept and generate a question
-        concept = random.choice(concepts)
-        # query index
-        query_engine = index.as_query_engine()
-        query = f"What is {concept}?"
-        facts = query_engine.query(query)
+        # Step 3: Generate Questions Based on Skill Level
+        question_prompt = generate_quiz_question_prompt(concept, taxonomy)
 
-        query = (f"Given the information: {facts} \n\n Write me a quiz "
-                 f"question-answer pair based on the concept {concept}. "
-                 f"Format it as a python dictionary with keys 'question' and "
-                 f"'answer' that will work with the eval() function." +
-                 "For example: ```python \n {'question': 'What is the color "
-                 "of a dove?, 'answer': 'White'}\n```")
-        response = llm.complete(query)
+        question_prompt += (
+            ' \nFormat the response as a python dictionary. For example: {'
+            '"question": "What is the capital of France?", "answer": "Paris"}'
+        )
+
+        # Step 4: Generate Questions and Answers
+        response = llm.chat([
+            ChatMessage(role='system', content='You are a teacher preparing a quiz for your students.'),
+            ChatMessage(role='user', content=question_prompt)
+        ])
+
+        # Extract the question and answer from the response
         try:
-            qa = eval(response.text.split('python')[-1].replace('`',''))
+            qa = eval(response.message.content.replace('`','').split('python')[-1])
         except:
-            print("Error in response:", response.choices[0].text)
+            print(f"Error generating question {i+1}. Skipping...")
             continue
-
-        # Append question-answer pair
-        questions_answers.append(qa)
-
-    # Display the generated questions
-    for i, qa in enumerate(questions_answers, 1):
+        questions_list.append(qa)
         print(f"Q{i}: {qa['question']}")
         print(f"A{i}: {qa['answer']}")
         print()
 
+
     # Save to CSV file using pandas
     filename = os.path.join(output_local, f"week_{week_number}_quiz.csv")
-    df = pd.DataFrame(questions_answers)
+    df = pd.DataFrame(questions_list)
     df.to_csv(filename, index=False)
 
     print(f"Quiz questions saved to {filename}")
