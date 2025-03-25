29740c2f72dc77c1dd15f6b0ef6ed1b69cbf54ac
gkuling <34655398+gkuling@users.noreply.github.com>
2025-03-25 15:45:06 -0400

    Revert "Finished this runs great"
    This reverts commit b04ee97b73945ee055d5ef03e4c4dd9610300c43.

--- BTQ_prompt.py
+++ BTQ_prompt.py
@@ -52,13 +52,13 @@ def generate_quiz_question_prompt(topic, topic_type, bloom_level, rag_gen):
             topic_preamble +
             f"And the following facts: \n{facts}\n\n" +
             bloom_instructions_objective[bloom_level].format(topic=topic) +
-            " Provide a detailed answer to this question a graduate student would respond with in 200 words or less."
+            " Provide a detailed answer to this question a graduate student would respond with."
         )
     elif topic_type == "article":
         prompt = (
             topic_preamble +
             f"And the following facts: \n{facts}\n\n" +
             bloom_instructions_reference[bloom_level].format(topic=topic) +
-            " Provide a detailed answer to this question a graduate student would respond with in 200 words or less."
+            " Provide a detailed answer to this question a graduate student would respond with."
         )
     return prompt
